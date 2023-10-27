3551584e3da86be268a0b51386e1d50c78bc5ce1
Vincent Lin <vinlin24@outlook.com>
2023-10-26 22:23:05 -0700

    Remove redundant and verbose ALUOperation::

--- ALU.cpp
+++ ALU.cpp
@@ -12,19 +12,19 @@ int32_t ALU::compute(int32_t input1, int32_t input2, ALUOperation operation)
     int32_t result;
     switch (operation)
     {
-    case ALUOperation::ALU_ADD:
+    case ALU_ADD:
         result = input1 + input2;
         break;
-    case ALUOperation::ALU_SUB:
+    case ALU_SUB:
         result = input1 - input2;
         break;
-    case ALUOperation::ALU_XOR:
+    case ALU_XOR:
         result = input1 ^ input2;
         break;
-    case ALUOperation::ALU_SRA:
+    case ALU_SRA:
         result = input1 >> (input2 & 0b11111);
         break;
-    case ALUOperation::ALU_AND:
+    case ALU_AND:
         result = input1 & input2;
         break;
     default:
@@ -46,24 +46,24 @@ ALUControl::resolveOperation(ALUOp op, InstructionParts const &parts) const
     switch (op)
     {
     case ALUOp::ADD:
-        return ALUOperation::ALU_ADD;
+        return ALU_ADD;
     case ALUOp::SUB:
-        return ALUOperation::ALU_SUB;
+        return ALU_SUB;
     case ALUOp::FUNC:
         switch (parts.funct3.to_ulong())
         {
         case 0b000:
             if (parts.opcode == Opcodes::I_TYPE) // addi
-                return ALUOperation::ALU_ADD;
+                return ALU_ADD;
             if (parts.bit30)
-                return ALUOperation::ALU_SUB;
-            return ALUOperation::ALU_ADD;
+                return ALU_SUB;
+            return ALU_ADD;
         case 0b100:
-            return ALUOperation::ALU_XOR;
+            return ALU_XOR;
         case 0b101:
-            return ALUOperation::ALU_SRA;
+            return ALU_SRA;
         case 0b111:
-            return ALUOperation::ALU_AND;
+            return ALU_AND;
         default:
             // Could not resolve operation with ALUOp=FUNC.
             assert(false);
